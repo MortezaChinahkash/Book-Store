04f159c3d904ddf856cebe0f61d20c5566e52cb7
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-03-13 22:01:15 +0100

    added the Book information and added toFixed() to price

--- js/script.js
+++ js/script.js
@@ -10,13 +10,27 @@ function renderBookWindow(){
                 <img class="book_pic" src="./assets/png/book-306468_640.png">
             </div>
             <div class="price_likes">
-                <h2 class="price">${books[i].price} €</h2> 
+                <h2 class="price">${books[i].price.toFixed(2)} €</h2> 
                 <div class="likebox">
                     <p class="likes">${books[i].likes}</p>
                     <img class="heart"src="./assets/png/favorite_50dp_E3E3E3_FILL0_wght400_GRAD0_opsz48.png">
                 </div>
             </div>
             <div class="book_info">
+                <table>
+                    <tr>
+                        <td class="td_left">Author:</td>
+                        <td class="td_right">${books[i].author}</td>
+                    </tr>
+                    <tr>
+                        <td class="td_left">Erscheinungsjahr:</td>
+                        <td class="td_right">${books[i].publishedYear}</td>
+                    </tr>
+                    <tr>
+                        <td class="td_left">Genre:</td>
+                        <td class="td_right">${books[i].genre}</td>
+                    </tr>
+                </table>
             </div>
 
         
