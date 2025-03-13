14e8af9e83cf276ee413be1017cb39843f4f7038
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-03-13 21:17:29 +0100

    integrated the Book titles

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ function renderBookWindow(){
     for (let i = 0; i < books.length; i++) {
         const element = books[i];
         document.getElementById("bookWindow").innerHTML += /*html*/`
-        <div class="book_ui"></div>
+        <div class="book_ui">
+            <div class="border_bottom">
+                <h2 class="title_of_book">${books[i].name}<h2>
+            <div>
+            
+        
+        
+        </div>
         `
         
     }
